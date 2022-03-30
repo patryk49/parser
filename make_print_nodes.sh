43141bb87a6b9cd921f2b62bc27b06f89ffcc5c5
patryk49 <101907914+patryk49@users.noreply.github.com>
2022-03-30 12:04:15 +0200

    Update and rename maker.sh to make_print_nodes.sh

---
+++ make_print_nodes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ print_nodes.cpp -o print_nodes -g -std=c++20 -Iinclude -fno-exceptions
--- maker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ main.cpp -o main -O3 -std=c++20 -Iinclude -fno-exceptions
